a396e30dda747ca7fd52b705013348f216f39f0d
changlz <changlizhi@aliyun.com>
2017-09-12 11:52:43 +0800

    自动生成main控制器

--- zf/Bianliang.go
+++ zf/Bianliang.go
@@ -10,3 +10,7 @@ func (zf *Zf) Hanfuxinormer(xiaoxie bool) string {
 func (zf *Zf) Cuowu(xiaoxie bool) string {
 	return fanshe.Fangfaming(xiaoxie)
 }
+
+func (zf *Zf) Mokuaimings(xiaoxie bool) string {
+	return fanshe.Fangfaming(xiaoxie)
+}
