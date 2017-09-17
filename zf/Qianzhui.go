6642b9352b1cc8427116df1926feae88a4ed0eb9
changlz <changlizhi@aliyun.com>
2017-09-17 16:48:19 +0800

    前缀Zuzhuang

--- zf/Qianzhui.go
+++ zf/Qianzhui.go
@@ -5,3 +5,6 @@ import "changliang/fanshe"
 func (zf *Zf) DT(xiaoxie bool) string {
 	return fanshe.Fangfaming(xiaoxie)
 }
+func (zf *Zf) Zuzhuang(xiaoxie bool) string {
+	return fanshe.Fangfaming(xiaoxie)
+}
