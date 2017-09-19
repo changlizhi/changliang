0f2194195619ff5a2553a07e3d3722a052c942b6
changlz <changlizhi@aliyun.com>
2017-09-19 15:14:02 +0800

    es6的常量值

---
+++ zf/Js.go
@@ -0,0 +1,15 @@
+package zf
+
+import (
+	"changliang/fanshe"
+)
+
+func (zf *Zf) Export(xiaoxie bool) string {
+	return fanshe.Fangfaming(xiaoxie)
+}
+func (zf *Zf) Function(xiaoxie bool) string {
+	return fanshe.Fangfaming(xiaoxie)
+}
+func (zf *Zf) Lieming(xiaoxie bool) string {
+	return fanshe.Fangfaming(xiaoxie)
+}
