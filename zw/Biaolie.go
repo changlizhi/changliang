28fd92639dcfebc2932800643cfde8f5b29b25b5
changlz <changlizhi@aliyun.com>
2017-10-12 15:45:35 +0800

    表列的中文说明

--- zw/Biaolie.go
+++ zw/Biaolie.go
@@ -69,3 +69,278 @@ func (zw *Zw) Chuangjianriqi() string {
 func (zw *Zw) Xiugairiqi() string {
 	return "修改日期"
 }
+
+
+
+func (zw *Zw) Jueseziyuan() string {
+	return "角色资源"
+}
+func (zw *Zw) Zhanghaojuese() string {
+	return "帐号角色"
+}
+func (zw *Zw) Ziyuan() string {
+	return "资源"
+}
+func (zw *Zw) Shebei() string {
+	return "设备"
+}
+func (zw *Zw) Yinpin() string {
+	return "音频"
+}
+func (zw *Zw) Yinpinbofang() string {
+	return "音频播放"
+}
+func (zw *Zw) Yinpinxiazai() string {
+	return "音频下载"
+}
+func (zw *Zw) Xiangmu() string {
+	return "项目"
+}
+func (zw *Zw) Xiangmuzu() string {
+	return "项目组"
+}
+func (zw *Zw) Xiangmuzuxiangmu() string {
+	return "项目组项目"
+}
+func (zw *Zw) Shijian() string {
+	return "时间"
+}
+func (zw *Zw) Zhanghaoshijian() string {
+	return "帐号时间"
+}
+func (zw *Zw) Zhanghao() string {
+	return "帐号"
+}
+func (zw *Zw) Zidian() string {
+	return "字典"
+}
+func (zw *Zw) Yonghu() string {
+	return "用户"
+}
+func (zw *Zw) Yonghujibing() string {
+	return "用户疾病"
+}
+func (zw *Zw) Wenzhang() string {
+	return "文章"
+}
+func (zw *Zw) Liuyan() string {
+	return "留言"
+}
+func (zw *Zw) Fuwufankui() string {
+	return "服务反馈"
+}
+func (zw *Zw) Xiangmushuju() string {
+	return "项目数据"
+}
+func (zw *Zw) Xiangmuzushijian() string {
+	return "项目组时间"
+}
+func (zw *Zw) Jibing() string {
+	return "疾病"
+}
+func (zw *Zw) Cujinfangan() string {
+	return "促进方案"
+}
+func (zw *Zw) Jiazubingshi() string {
+	return "家族病史"
+}
+func (zw *Zw) Buliangfanying() string {
+	return "不良反应"
+}
+func (zw *Zw) Qita() string {
+	return "其他"
+}
+func (zw *Zw) Exingzhongliu() string {
+	return "恶性肿瘤"
+}
+func (zw *Zw) Ganran() string {
+	return "感染"
+}
+func (zw *Zw) Pifubing() string {
+	return "皮肤病"
+}
+func (zw *Zw) Xinnaoxueguanbing() string {
+	return "心脑血管病"
+}
+func (zw *Zw) Ganyu() string {
+	return "干预"
+}
+func (zw *Zw) Jiliang() string {
+	return "剂量"
+}
+func (zw *Zw) Zhusheliang() string {
+	return "注射量"
+}
+func (zw *Zw) Zhushe() string {
+	return "注射"
+}
+func (zw *Zw) Fuyaoliang() string {
+	return "服药量"
+}
+func (zw *Zw) Fuyao() string {
+	return "服药"
+}
+func (zw *Zw) Tanghuaxuehongdanbai() string {
+	return "糖化血红蛋白"
+}
+func (zw *Zw) Canhouerxiaoshi() string {
+	return "餐后二小时"
+}
+func (zw *Zw) Kongfu() string {
+	return "空腹"
+}
+func (zw *Zw) Tangniaobing() string {
+	return "糖尿病"
+}
+func (zw *Zw) Linchuangzhenduan() string {
+	return "临床诊断"
+}
+func (zw *Zw) Jiankangzhuangkuang() string {
+	return "监控状况"
+}
+func (zw *Zw) Jiankangtouzi() string {
+	return "健康投资"
+}
+func (zw *Zw) Yundongshijian() string {
+	return "运动时间"
+}
+func (zw *Zw) Yundongcishu() string {
+	return "运动次数"
+}
+func (zw *Zw) Yundongleixing() string {
+	return "运动类型"
+}
+func (zw *Zw) Shifouhejiu() string {
+	return "是否喝酒"
+}
+func (zw *Zw) Shifouxiyan() string {
+	return "是否吸烟"
+}
+func (zw *Zw) Shuimianzhuangtai() string {
+	return "睡眠状态"
+}
+func (zw *Zw) Yinshixiguan() string {
+	return "饮食习惯"
+}
+func (zw *Zw) Hunfou() string {
+	return "婚否"
+}
+func (zw *Zw) Wenhuachengdu() string {
+	return "文化程度"
+}
+func (zw *Zw) Chushengriqi() string {
+	return "出生日期"
+}
+func (zw *Zw) Minzu() string {
+	return "民族"
+}
+func (zw *Zw) Tizhong() string {
+	return "体重"
+}
+func (zw *Zw) Shengao() string {
+	return "身高"
+}
+func (zw *Zw) Zhiye() string {
+	return "职业"
+}
+func (zw *Zw) Nianling() string {
+	return "年龄"
+}
+func (zw *Zw) Xingbie() string {
+	return "性别"
+}
+func (zw *Zw) Qidongriqi() string {
+	return "启动日期"
+}
+func (zw *Zw) Touxiang() string {
+	return "头像"
+}
+func (zw *Zw) Shijianbianma() string {
+	return "时间编码"
+}
+func (zw *Zw) Qiyong() string {
+	return "启动"
+}
+func (zw *Zw) Xiangmuzubianma() string {
+	return "项目组编码"
+}
+func (zw *Zw) Yixuebianma() string {
+	return "医学编码"
+}
+func (zw *Zw) Xiangmubianma() string {
+	return "项目编码"
+}
+func (zw *Zw) Danwei() string {
+	return "单位"
+}
+func (zw *Zw) Jiamifangshi() string {
+	return "加密方式"
+}
+func (zw *Zw) Shebeihao() string {
+	return "设备号"
+}
+func (zw *Zw) Weiyibianma() string {
+	return "唯一编码"
+}
+func (zw *Zw) Miaoshu() string {
+	return "描述"
+}
+func (zw *Zw) Caozuoren() string {
+	return "操作人"
+}
+func (zw *Zw) Youxiaoxing() string {
+	return "有效性"
+}
+func (zw *Zw) Caozuoriqi() string {
+	return "操作日期"
+}
+func (zw *Zw) Neirong() string {
+	return "内容"
+}
+func (zw *Zw) Shifoufabu() string {
+	return "是否发布"
+}
+func (zw *Zw) Bofangshijian() string {
+	return "播放时间"
+}
+func (zw *Zw) Jinzhixiazai() string {
+	return "禁止下载"
+}
+func (zw *Zw) Yinpinbianma() string {
+	return "音频编码"
+}
+func (zw *Zw) Ziyuanbianma() string {
+	return "资源编码"
+}
+func (zw *Zw) Fuwuganshou() string {
+	return "服务感受"
+}
+func (zw *Zw) Shentiganshou() string {
+	return "身体感受"
+}
+func (zw *Zw) Xiaoguokeping() string {
+	return "效果客评"
+}
+func (zw *Zw) Cankao() string {
+	return "参考"
+}
+func (zw *Zw) Zhidingren() string {
+	return "制定人"
+}
+func (zw *Zw) Xuetang() string {
+	return "血糖"
+}
+func (zw *Zw) Xuetangchuli() string {
+	return "血糖处理"
+}
+
+func (zw *Zw) Biaoti() string {
+	return "标题"
+}
+
+func (zw *Zw) Huifuyonghu() string {
+	return "回复用户"
+}
+func (zw *Zw) Liuyanyonghu() string {
+	return "留言用户"
+}
