95a9cf0ce160ca472e2fd3dd3e1e6053575437be
changlz <changlizhi@aliyun.com>
2017-10-13 10:15:40 +0800

    增加根据interface返回值

--- tests/Zfzhi_test.go
+++ tests/Zfzhi_test.go
@@ -9,3 +9,10 @@ import (
 func TestPrintzhi(t *testing.T) {
 	log.Println(zfzhi.Zhi.Baifenhao())
 }
+
+func TestIshuzi1(t *testing.T) {
+	zi := zfzhi.Zhi.Ishuzi1(false)
+	zs := zfzhi.Zhi.Ishuzi1(true)
+	log.Println(zi.(int))
+	log.Println(zs.(string))
+}
