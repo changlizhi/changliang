9cab51c22d10f2a81d3a9c79605c2f71553e594f
changlz <changlizhi@aliyun.com>
2017-10-14 17:58:03 +0800

    增加中文显示

---
+++ zw/Peizhi.go
@@ -0,0 +1,32 @@
+package zw
+
+func (zw *Zw) Yuyan() string {
+	return "语言"
+}
+func (zw *Zw) Yu() string {
+	return "本地域"
+}
+func (zw *Zw) Duankou() string {
+	return "端口"
+}
+func (zw *Zw) Jwtmima() string {
+	return "Jwt密码"
+}
+func (zw *Zw) Jwtyan() string {
+	return "Jwt盐"
+}
+func (zw *Zw) Tiaoshu() string {
+	return "条数"
+}
+func (zw *Zw) Ormdebug() string {
+	return "Ormdebug开启与否"
+}
+func (zw *Zw) Ip() string {
+	return "Ip地址"
+}
+func (zw *Zw) Qudong() string {
+	return "驱动"
+}
+func (zw *Zw) Mima() string {
+	return "密码"
+}
