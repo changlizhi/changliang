49aa5fdffc109f5f32f8ca8f6a6a00c83eccbcc3
changlz <changlizhi@aliyun.com>
2017-10-15 14:44:40 +0800

    提示和错误的标题常量

---
+++ zw/Cuowu.go
@@ -0,0 +1,16 @@
+package zw
+func (zw *Zw) Error01() string {
+	return "系统异常提示"
+}
+func (zw *Zw) Error02() string {
+	return "json解析错误提示"
+}
+func (zw *Zw) Error03() string {
+	return "字段类型错误提示"
+}
+func (zw *Zw) Error04() string {
+	return "数据不存在提示"
+}
+func (zw *Zw) Error05() string {
+	return "Id类型错误提示"
+}
---
+++ zw/Tishi.go
@@ -0,0 +1,29 @@
+package zw
+
+func (zw *Zw) Tishi01() string {
+	return "邮箱格式提示"
+}
+func (zw *Zw) Tishi02() string {
+	return "密码格式提示"
+}
+func (zw *Zw) Tishi03() string {
+	return "保存成功提示"
+}
+func (zw *Zw) Tishi04() string {
+	return "保存失败提示"
+}
+func (zw *Zw) Tishi05() string {
+	return "修改成功提示"
+}
+func (zw *Zw) Tishi06() string {
+	return "修改失败提示"
+}
+func (zw *Zw) Tishi07() string {
+	return "删除成功提示"
+}
+func (zw *Zw) Tishi08() string {
+	return "删除失败提示"
+}
+func (zw *Zw) Tishi09() string {
+	return "验证失败提示"
+}
