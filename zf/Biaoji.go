95fcfdde6be4e837013bf370f490cd783abe7472
changlz <changlizhi@aliyun.com>
2017-10-19 15:25:28 +0800

    增加表的标记两个

--- zf/Biaoji.go
+++ zf/Biaoji.go
@@ -16,3 +16,10 @@ func (zf *Zf) Zhengchang(xiaoxie bool) string {
 func (zf *Zf) Wancheng(xiaoxie bool) string {
 	return fanshe.Fangfaming(xiaoxie)
 }
+func (zf *Zf) Yiban(xiaoxie bool) string {//一般的数据表都不做特殊处理
+	return fanshe.Fangfaming(xiaoxie)
+}
+func (zf *Zf) Yemianlianjie(xiaoxie bool) string {
+	//这个标记是给ziyuan表用的，如果是资源表则生成一个特别的控制器展示数据
+	return fanshe.Fangfaming(xiaoxie)
+}
